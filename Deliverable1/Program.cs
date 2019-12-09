5f25316936987637b05feda0a2a2f683d725aeac
Corey <coreyforbes864@gmail.com>
2019-12-08 19:48:43 -0500

    first commit for deliverable one

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tropical;
+            Console.Write("what kind of trip would you like to go on, musical, tropical, or adventurous? ");
+            tropical = Console.ReadLine();
+            Console.ReadKey();
+            {
+                
+                Console.Write("How many are in your group? ");
+                Console.ReadLine();
+
+                {
+
+                    Console.Write("since you're a group of 2 going on a tropical vacation, you should take a first class flight to a beaach vacation in mexico ");
+                    Console.ReadLine();
+
+
+
+                }
+                
+
+
+            }
+
+
+
+        }
+    }
+}
